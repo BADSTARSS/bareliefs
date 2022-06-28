4d29c00c419dca0aa682900d7726c02194deadfb
bunsong songprathipkun <t00930@g-able.com>
2022-06-28 16:42:12 +0700

    updatv.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { WheretobuyComponent } from './wheretobuy/wheretobuy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'wheretobuy', component: WheretobuyComponent },
+  { path: 'product', component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  title = 'Barelife';
+}
